8250a372a8a19d9c7afa9f2bafeeabd36ee30512
Daniel Duque <daniel.duque@cern.ch>
2025-05-18 00:00:56 -0700

    refactor: Change Huber loss for L1 loss

--- dev/training/loss.py
+++ dev/training/loss.py
@@ -8,8 +8,7 @@ class CustomLoss(nn.Module):
 
     Args:
         config (dict): Loss function configuration with keys:
-            - `delta` (float): Threshold at which to switch between delta-scaled
-              L1 and L2 loss.
+            -
 
     Inputs:
         pred (Tensor): Predicted values of shape (B,).
@@ -22,8 +21,6 @@ class CustomLoss(nn.Module):
     def __init__(self, config):
         super().__init__()
 
-        self.delta = config["delta"]
-
     def forward(self, pred, target):
-        huber = F.huber_loss(pred, target, delta=self.delta)
-        return huber
+        loss = F.l1_loss(pred, target)
+        return loss
