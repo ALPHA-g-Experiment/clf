97deab4fb874adb831e51db8c6a41782b696d6e2
Daniel Duque <daniel.duque@cern.ch>
2025-07-20 23:15:12 -0700

    feat: Add clf preprocessing script

--- dev/preprocess.py
+++ dev/preprocess.py
@@ -9,36 +9,28 @@ parser = argparse.ArgumentParser(
     description="Dataset preprocessing",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
-parser.add_argument("dataset", help="path to the Parquet dataset")
+parser.add_argument("pbar_dataset", help="path to the signal Parquet dataset")
+parser.add_argument("cosmic_dataset", help="path to the background Parquet dataset")
 parser.add_argument(
     "weights",
     type=int,
     nargs="+",
     help="integer weights for dataset splits (e.g. 1 1 for 50/50)",
 )
-parser.add_argument("--bins", type=int, default=100, help="number of flattening bins")
 parser.add_argument("--output-dir", default=".", help="output directory")
 
 args = parser.parse_args()
 
-# Flatten the full dataset before splitting to avoid losing more data than
-# necessary by e.g. fluctuation in binning in smaller subsets
-lf = pl.scan_parquet(args.dataset).filter(pl.col("point_cloud").list.len() > 0)
-z = lf.select(pl.col("target").arr.get(2)).collect()
-breaks = pl.linear_space(
-    z.min().item(), z.max().item(), num_samples=args.bins - 1, closed="none", eager=True
-)
-lf = lf.with_columns(bin=pl.col("target").arr.get(2).cut(breaks))
+signal_df = pl.scan_parquet(args.pbar_dataset).with_columns(target=1.0).collect()
+background_df = pl.scan_parquet(args.cosmic_dataset).with_columns(target=0.0).collect()
 
-min_len = lf.group_by("bin").len().select(pl.col("len").min()).collect().item()
-flat_lf = lf.filter(pl.int_range(pl.len()).shuffle(seed=0).over("bin") < min_len).drop(
-    "bin"
-)
+n_rows = min(signal_df.height, background_df.height)
+signal_df = signal_df.sample(n_rows, shuffle=True, seed=0)
+background_df = background_df.sample(n_rows, shuffle=True, seed=0)
 
 total_counts = Counter(args.weights)
 counter = Counter()
 
-rows = flat_lf.select(pl.len()).collect().item()
 offset = 0
 for weight in args.weights:
     suffix = (
@@ -46,10 +38,15 @@ for weight in args.weights:
         if total_counts[weight] == 1
         else f"{weight}{chr(97 + counter[weight])}"
     )
-    output = Path(args.output_dir) / f"{Path(args.dataset).stem}_{suffix}.parquet"
+    output = (
+        Path(args.output_dir)
+        / f"{Path(args.pbar_dataset).stem}_{Path(args.cosmic_dataset).stem}_{suffix}.parquet"
+    )
 
-    length = int(rows * weight / sum(args.weights))
-    flat_lf.slice(offset, length).sink_parquet(output)
+    length = int(n_rows * weight / sum(args.weights))
+    pl.concat(
+        [signal_df.slice(offset, length), background_df.slice(offset, length)]
+    ).sample(fraction=1.0, shuffle=True, seed=0).write_parquet(output)
     print(f"Created `{output}`")
 
     counter[weight] += 1
