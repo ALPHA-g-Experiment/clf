c437d71ef48f075bb3afe6c0f8cc2b164a2c382e
Daniel Duque <daniel.duque@cern.ch>
2025-07-24 20:45:57 -0700

    feat: Add ROC curve visualization

--- dev/visualize.py
+++ dev/visualize.py
@@ -27,6 +27,25 @@ def scores(data, bins, min, max, normalize, alpha):
     )
 
 
+def roc(data):
+    df = pl.read_csv(data)
+
+    n_signal = df.filter(pl.col("target") == 1.0).height
+    n_background = df.filter(pl.col("target") == 0.0).height
+
+    thresholds = np.linspace(0, 1, 100)
+    true_positives = np.zeros_like(thresholds)
+    false_positives = np.zeros_like(thresholds)
+
+    for i, threshold in enumerate(thresholds):
+        passed = df.filter(pl.col("prediction") >= threshold)
+
+        true_positives[i] = passed.filter(pl.col("target") == 1.0).height
+        false_positives[i] = passed.filter(pl.col("target") == 0.0).height
+
+    plt.plot(false_positives / n_background, true_positives / n_signal, label=data)
+
+
 parser = argparse.ArgumentParser(description="Data Visualization")
 subparsers = parser.add_subparsers(dest="subcommand", required=True)
 
@@ -50,6 +69,12 @@ parser_scores = subparsers.add_parser(
     parents=[parser_all, parser_hist],
 )
 
+parser_roc = subparsers.add_parser(
+    "roc",
+    help="ROC curve (expects CSV test results)",
+    parents=[parser_all],
+)
+
 args = parser.parse_args()
 
 alpha = 1.0 if len(args.data) == 1 else 0.5
@@ -59,6 +84,10 @@ for dataset in args.data:
             scores(dataset, args.bins, args.min, args.max, args.normalize, 0.6)
             plt.xlabel("Normalized logit")
             plt.ylabel("Count")
+        case "roc":
+            roc(dataset)
+            plt.xlabel("1 - Specificity")
+            plt.ylabel("Sensitivity")
 
 plt.legend()
 
